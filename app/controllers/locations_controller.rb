5ac29b0ea48e2fbea1ea911e4a2d15160e766dea
Derrick Cheng <dcheng090@gmail.com>
2012-04-02 07:14:56 -0700

    fixed locations maybe

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -37,13 +37,12 @@ class LocationsController < ApplicationController
   end
 
   def setDistance(loc)
-    if User.first
-    else
+    if Loc.first==Loc.last
       loc.dist=0
       loc.save
       return
     end
-    if loc.number==User.first.number
+    if User.first!=nil and loc.number==User.first.number
       locations=Location.all
     else
       locations=[loc]
